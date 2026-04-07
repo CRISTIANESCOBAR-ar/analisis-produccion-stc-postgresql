713f4d5a586e10f6fab0c01bc814dd20f931f921
CRISTIANESCOBAR-ar <cristianescobar@hotmail.com>
2026-04-07 12:35:11 -0300

    build: actualiza sw.js (build dist)

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-46f6dd99'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i4rdk2f1on8"
+    "revision": "0.v8e7uvcaatk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
